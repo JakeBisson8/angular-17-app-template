f06aeb4ea095108e600f0e32891093fa79799d2f
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2023-12-13 17:24:18 -0500

    add prettier to lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,4 @@
 module.exports = {
+  '*': 'prettier --write --ignore-unknown',
   'src/**/*.{ts,html}': (files) => `ng lint --fix ${files.map((file) => `--lint-file-patterns ${file}`).join(' ')}`,
 };
