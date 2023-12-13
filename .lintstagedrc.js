77f3dc38c0a09b7359b2b6c610e371026ec257cc
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2023-12-13 17:18:02 -0500

    remove prettier from lint-staged as it hangs on a single file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,3 @@
 module.exports = {
-  '*': 'prettier --write --ignoreunknown',
   'src/**/*.{ts,html}': (files) => `ng lint --fix ${files.map((file) => `--lint-file-patterns ${file}`).join(' ')}`,
 };
