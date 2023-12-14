ab20d68669ce8f2c18d6213b515b92f2d38edc42
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2023-12-14 10:32:18 -0500

    cleanup

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,3 @@
 module.exports = {
-  // '*': 'prettier --write --ignore-unknown',
-  // 'src/**/*.{ts,html}': (files) => `ng lint --fix ${files.map((file) => `--lint-file-patterns ${file}`).join(' ')}`,
   'src/**/*.{ts,html}': ['prettier --write --ignore-unknown', 'eslint --fix'],
 };
