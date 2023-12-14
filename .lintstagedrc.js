b12f9a985cbf1e51edeef0664a99bf9c05de3cc5
Jake Bisson <Jake.Bisson@nrc-cnrc.gc.ca>
2023-12-14 10:31:25 -0500

    changed lint-staged to run eslint instead of ng lint as running prettier and eslint in parralel may have been conflicting and causing prettier step to hang

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-  '*': 'prettier --write --ignore-unknown',
-  'src/**/*.{ts,html}': (files) => `ng lint --fix ${files.map((file) => `--lint-file-patterns ${file}`).join(' ')}`,
+  // '*': 'prettier --write --ignore-unknown',
+  // 'src/**/*.{ts,html}': (files) => `ng lint --fix ${files.map((file) => `--lint-file-patterns ${file}`).join(' ')}`,
+  'src/**/*.{ts,html}': ['prettier --write --ignore-unknown', 'eslint --fix'],
 };
